196ce0907721337a91cc070c8665e0e0779a5276
Anuja-2000 <anujawij@gmail.com>
2024-11-30 12:24:59 +0530

    Wrote sample test scenario for hosted ui

--- cypress-cucumber/cypress.config.js
+++ cypress-cucumber/cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
-    },
-    baseUrl: "http://localhost:3000"
+    }
   },
 });
--- cypress-cucumber/cypress/e2e/home/home.js
+++
@@ -1,9 +0,0 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-
-Given('I visit the {string} page', (page) => {
-  cy.visit(page);
-});
-
-Then('I should see an {string} tag containing {string}', (tag, text) => {
-  cy.get(tag).contains(text).should('be.visible');
-});
