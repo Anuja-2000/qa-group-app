0fb8f9da7bbdd2df34e36502032bee3b261aa12a
Dasuni Rathnayaka <rathnayakaamdb.20@uom.lk>
2024-12-27 10:02:14 +0530

    Updated CheckoutOverviewTest using OverviewPage methods

--- cypress-cucumber/cypress/e2e/Tests/CheckoutOverviewTest/CheckoutOverviewTest.cy.js
+++ cypress-cucumber/cypress/e2e/Tests/CheckoutOverviewTest/CheckoutOverviewTest.cy.js
@@ -1,7 +1,9 @@
-import {Given, When, Then, And} from "cypress-cucumber-preprocessor/steps"
+import {Given, When, Then} from "cypress-cucumber-preprocessor/steps";
 import Inventory from "../../Pages/inventoryPage/InventoryPage.cy";
+import OverviewPage from "../../Pages/OverviewPage/OverviewPage.cy";
 
 const inventory = new Inventory();
+const overviewPage = new OverviewPage();
 
 Given('the user is logged in', () => {
     cy.login('standard_user', 'secret_sauce');
@@ -31,59 +33,45 @@ When('the user proceeds to checkout overview', () => {
     cy.get('#first-name').type('John');
     cy.get('#last-name').type('Doe');
     cy.get('#postal-code').type('12345');
-    cy.get('input[value="Continue"]').click();
+    cy.get('input[value="Continue"]').click();    
 });
 
 Then('the user should see the "Item total: ${float}" information', (expectedTotal) => {
-    cy.get('.summary_subtotal_label').should('contain.text', `Item total: $${expectedTotal}`);
+    overviewPage.verifyItemTotal(expectedTotal);
 });
 
 Then('the user should see the "Item total: $0" information', () => {
-    cy.get('.summary_subtotal_label').should('contain.text', 'Item total: $0');
+    overviewPage.verifyItemTotal(0);
 });
 
 Then('the user should see the "Tax: ${float}" information', (expectedTax) => {
-    cy.get('.summary_tax_label').should('contain.text', `Tax: $${expectedTax}`);
+    overviewPage.verifyTax(expectedTax);
 });
 
 Then('the user should see the "Total: ${float}" information', (expectedTotal) => {
-    cy.get('.summary_total_label').should('contain.text', `Total: $${expectedTotal}`);
+    overviewPage.verifyTotal(expectedTotal);
 });
 
 Then('the user should see the "Payment Information: SauceCard #31337" information', () => {
-    cy.get('.summary_value_label[data-test="payment-info-value"]')
-        .should('contain.text', 'SauceCard #31337');
+    overviewPage.verifyPaymentInformation();
 });
 
 Then('the user should see the "Shipping Information: Free Pony Express Delivery!" information', () => {
-    cy.get('.summary_value_label[data-test="shipping-info-value"]')
-        .should('contain.text', 'Free Pony Express Delivery!');
+    overviewPage.verifyShippingInformation();
 });
 
 Then('the user should see the item title {string} for item {int}', (itemTitle, itemIndex) => {
-    cy.get('.cart_item')
-        .eq(itemIndex - 1)
-        .find('.inventory_item_name')
-        .should('have.text', `${itemTitle}`);
+    overviewPage.verifyItemTitle(itemTitle, itemIndex);
 });
 
 Then('the user should see the item description for Item {int}', (itemIndex) => {
-    cy.get('.cart_item')
-        .eq(itemIndex - 1)
-        .find('.inventory_item_desc')
-        .should('not.be.empty');
+    overviewPage.verifyItemDescriptionIsNotEmpty(itemIndex);
 });
 
 Then('the user should see the item price {string} for Item {int}', (itemPrice, itemIndex) => {
-    cy.get('.cart_item')
-        .eq(itemIndex - 1)
-        .find('.inventory_item_price')
-        .should('have.text', `${itemPrice}`);
+    overviewPage.verifyItemPrice(itemPrice, itemIndex);
 });
 
 Then('the user should see the item quantity "{int}" for Item {int}', (itemQty, itemIndex) => {
-    cy.get('.cart_item')
-        .eq(itemIndex - 1)
-        .find('.cart_quantity')
-        .should('have.text', `${itemQty}`);
+    overviewPage.verifyItemQuantity(itemQty, itemIndex);
 });
